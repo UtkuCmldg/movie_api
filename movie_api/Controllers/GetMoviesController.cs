cf8cd13cbb1d405333b94f06e83fa94ea0780513
Utku <utkucamlidag@hotmail.com>
2022-12-19 21:05:21 +0300

    Create default GetMovieDetails() and GetMovies() endpoints

---
+++ movie_api/Controllers/GetMoviesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace movie_api.Controllers
+{
+    [ApiController]
+    public class GetMoviesController : ControllerBase
+    {
+        private static string _sqlConnectionString = "server=localhost;database=dapper_example;uid=root;password=";
+
+
+        [Route("/api/get/movie-details")]
+        [HttpGet]
+        public string GetMovieDetails()
+        {
+            return "sadds";
+        }
+
+        [Route("/api/get/movies")]
+        [HttpGet]
+        public string GetMovies()
+        {
+            return "sadds";
+        }
+    }
+}
