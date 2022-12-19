b33e555397c1f44133169b639e722d5e125ac22a
Utku <utkucamlidag@hotmail.com>
2022-12-19 21:48:20 +0300

    mysql connection and dapper integration in GetMovieDetails() action

--- movie_api/Controllers/GetMoviesController.cs
+++ movie_api/Controllers/GetMoviesController.cs
@@ -1,19 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using movie_api.Models;
+using MySql.Data.MySqlClient;
 
 namespace movie_api.Controllers
 {
     [ApiController]
     public class GetMoviesController : ControllerBase
     {
-        private static string _sqlConnectionString = "server=localhost;database=dapper_example;uid=root;password=";
+        private static string _sqlConnectionString = "server=localhost;database=films_api_project;uid=root;password=";
 
 
         [Route("/api/get/movie-details")]
         [HttpGet]
-        public string GetMovieDetails()
+        public IActionResult GetMovieDetails(int movieId)
         {
-            return "sadds";
+            
+            using (var connection = new MySqlConnection(_sqlConnectionString))
+            {
+                //try to open connection with database
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception)
+                {
+
+                    return StatusCode(500, "Internal Server Error. Something Went Wrong !!");
+                }
+
+                //try to get movie from movies table
+                try
+                {
+                    var movie = connection.QuerySingle<MovieModel>("SELECT movies.movie_id, movies.budget, movies.overview, movies.title, movie_genres.movie_genre, movie_original_languages.movie_original_language_name  FROM movies INNER JOIN movie_genres ON movies.movie_genre_id = movie_genres.movie_genre_id INNER JOIN movie_original_languages ON movie_original_languages.movie_original_language_id = movies.movie_original_language_id WHERE movies.movie_id = @MovieId; ", new { MovieId = movieId });
+
+                    return Ok(movie);
+                }
+                catch (Exception)
+                {
+
+                    return StatusCode(404, "Not Found. Coudn't find anything that matches given movie id, try something else.");
+                }
+
+
+                connection.Close();
+
+                
+
+            }
+
         }
 
         [Route("/api/get/movies")]
---
+++ movie_api/Models/MovieModel.cs
@@ -0,0 +1,12 @@
+﻿namespace movie_api.Models
+{
+    public class MovieModel
+    {
+        public int Movie_Id { get; set; }
+        public int Budget { get; set; }
+        public string Overview { get; set; }
+        public string Title { get; set; }
+        public string Movie_Genre { get; set; }
+        public string Movie_Original_Language_Name { get; set; }
+    }
+}
